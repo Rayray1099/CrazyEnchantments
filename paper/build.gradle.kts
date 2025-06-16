eaf2a05463a2ecaa0dfbd911f03bc23cb87a693a
Ryder Belserion <contact@ryderbelserion.com>
2025-06-15 21:58:56 -0400

    Fix issue with file not found

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -14,8 +14,6 @@ repositories {
     maven("https://repo.bg-software.com/repository/api")
 
     maven("https://maven.enginehub.org/repo")
-
-    maven("https://repo.oraxen.com/releases")
 }
 
 dependencies {
